9c66a5031b42aed2c37bec7fdc2453d490285670
Julio <julio.trindade92@gmail.com>
2018-08-30 15:31:33 -0300

    4

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ $.getJSON("database.json", function(data) {
     saida += '<div class="row">';
     saida += '<div class="col-lg-4 band-img">';
     saida += '<img src="' + bands[i].picture + '" alt="' + bands[i].name + '" title="' + bands[i].name + '">';
+    saida += '<h6>' + bands[i].name + '</h6>';
     saida += '<h6> ' + bands[i].description + '</h6>';
     saida += '</div>';
     saida += '</div>';
