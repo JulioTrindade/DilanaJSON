5155462f53c7c9d10787b1b06d7357d2a4bb6874
Julio <julio.trindade92@gmail.com>
2018-08-31 17:46:43 -0300

    1

--- js/script.js
+++ js/script.js
@@ -6,9 +6,12 @@ $.getJSON("database.json", function(data) {
   for (i = 0; i < bands.length; i++) {
     saida += '<div class="row">';
     saida += '<div class="col-lg-4 band-img">';
-    saida += '<img src="' + bands[i].picture + '" alt="' + bands[i].name + '" title="' + bands[i].name + '">';
-    saida += '<h6>' + bands[i].name + '</h6>';
-    saida += '<h6> ' + bands[i].description + '</h6>';
+    saida += '<img src="' + bands[i].foto + '" alt="' + bands[i].nome + '" title="' + bands[i].nome + '">';
+    saida += '<h6>' + bands[i].nome + '</h6>';
+    saida += '<h6>' + bands[i].email + '</h6>';
+    saida += '<h6>' + bands[i].telefone + '</h6>';
+    saida += '<h6> ' + bands[i].CPF + '</h6>';
+    saida += '<h6>' + bands[i].matricula + '</h6>';
     saida += '</div>';
     saida += '</div>';
   }
