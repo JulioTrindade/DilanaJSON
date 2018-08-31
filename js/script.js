863351c97f935a4b03b06c179877ad9ce016980e
Julio <julio.trindade92@gmail.com>
2018-08-31 18:09:59 -0300

    1

--- js/script.js
+++ js/script.js
@@ -4,16 +4,24 @@ $.getJSON("database.json", function(data) {
   var bands = data.bands;
   var saida;
   for (i = 0; i < bands.length; i++) {
-    saida += '<div class="row">';
-    saida += '<div class="col-lg-4 band-img">';
-    saida += '<img src="' + bands[i].foto + '" alt="' + bands[i].nome + '" title="' + bands[i].nome + '">';
-    saida += '<h6>' + bands[i].nome + '</h6>';
-    saida += '<h6>' + bands[i].email + '</h6>';
-    saida += '<h6>' + bands[i].telefone + '</h6>';
-    saida += '<h6> ' + bands[i].CPF + '</h6>';
-    saida += '<h6>' + bands[i].matricula + '</h6>';
-    saida += '</div>';
-    saida += '</div>';
+    saida += '<table>';
+      saida += '<tr>';
+        saida += '<td><div class="row">';
+          saida += '<div class="col-lg-4 band-img">';
+          saida += '<img src="' + bands[i].foto + '" alt="' + bands[i].nome + '" title="' + bands[i].nome + '">';
+        saida += '</div>';
+        saida += '</div></td>';
+        saida += '<td class="dados"><h6>' + bands[i].nome + '</h6>';
+        saida += '<h6>' + bands[i].email + '</h6>';
+        saida += '<h6>' + bands[i].telefone + '</h6>';
+        saida += '<h6> ' + bands[i].CPF + '</h6>';
+        saida += '<h6>' + bands[i].matricula + '</h6>';
+        saida += '<h6>' + bands[i].ativo + '</h6>';
+        saida += '<h6>' + bands[i].sala + '</h6></td>';
+      saida += '</tr>';
+    saida += '</table>';
+    
+    
   }
 
   document.getElementById('tela').innerHTML = saida;
