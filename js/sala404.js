192273592d5fd810f30c362b0212f310af442a6c
Julio <julio.trindade92@gmail.com>
2018-09-04 17:34:24 -0300

    1

--- js/sala404.js
+++ js/sala404.js
@@ -11,12 +11,12 @@ $.getJSON("database.json", function(data) {
                 saida += '<img class = "fotos" src="' + bands[i].foto + '" alt="' + bands[i].nome + '" title="' + bands[i].nome + '">';
             saida += '</div>';
             saida += '</div></td>';
-            saida += '<td class="dados"><h6>Nome: ' + bands[i].nome + '</h6>';
-            saida += '<h6>Email: ' + bands[i].email + '</h6>';
-            saida += '<h6>Telefone: ' + bands[i].telefone + '</h6>';
-            saida += '<h6>CPF: ' + bands[i].CPF + '</h6>';
-            saida += '<h6>Matrícula: ' + bands[i].matricula + '</h6>';
-            saida += '<h6>Sala: ' + bands[i].sala + '</h6></td>';
+            saida += '<td class="dados"><b>Nome:</b> ' + bands[i].nome + '<br>';
+            saida += '<b>Email:</b> ' + bands[i].email + '<br>';
+            saida += '<b>Telefone:</b> ' + bands[i].telefone + '<br>';
+            saida += '<b>CPF:</b> ' + bands[i].CPF + '<br>';
+            saida += '<b>Matrícula:</b> ' + bands[i].matricula + '<br>';
+            saida += '<b>Sala:</b> ' + bands[i].sala + '<br></td>';
             saida += '</tr>';
             saida += '</table>';
         }
